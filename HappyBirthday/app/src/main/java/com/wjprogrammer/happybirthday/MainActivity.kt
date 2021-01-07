7c44ab8e508ef543236c4eeb8e93ca5ef10c15bb
jay_wu <jay_wu@gss.com.tw>
2021-01-07 17:14:41 +0800

    ✨ add new page 💄 main: font style, button 🐛 duplicated image name

--- HappyBirthday/app/src/main/java/com/wjprogrammer/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/wjprogrammer/happybirthday/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.wjprogrammer.happybirthday
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+const val EXTRA_MESSAGE = "com.wj.happy.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goDiceRoller(view: View) {
+        val message = "val editText = findViewById<EditText>(R.id.editText)"
+        val intent = Intent(this, DiceRollerActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+
     }
 }
