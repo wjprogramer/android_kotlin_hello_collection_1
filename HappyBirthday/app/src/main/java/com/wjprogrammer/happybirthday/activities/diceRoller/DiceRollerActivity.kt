3de14c2ec685739c178473539b7d874814d4508a
jay_wu <jay_wu@gss.com.tw>
2021-01-08 17:32:57 +0800

    ✨ Finsih CodeLab: Create an interactive Dice Roller app

--- HappyBirthday/app/src/main/java/com/wjprogrammer/happybirthday/activities/diceRoller/DiceRollerActivity.kt
+++ HappyBirthday/app/src/main/java/com/wjprogrammer/happybirthday/activities/diceRoller/DiceRollerActivity.kt
@@ -2,11 +2,47 @@ package com.wjprogrammer.happybirthday.activities.diceRoller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.wjprogrammer.happybirthday.R
+import com.wjprogrammer.happybirthday.models.Dice
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class DiceRollerActivity : AppCompatActivity() {
+
+    /**
+     * This method is called when the Activity is created.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dice_roller)
+
+        // Find the Button in the layout
+        val rollButton: Button = findViewById(R.id.button)
+
+        // Set a click listener on the button to roll the dice when the user taps the button
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        Toast
+            .makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            .show()
+
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+
     }
 }
---
+++ HappyBirthday/app/src/main/java/com/wjprogrammer/happybirthday/models/Dice.kt
@@ -0,0 +1,14 @@
+package com.wjprogrammer.happybirthday.models
+
+/**
+ * Dice with a fixed number of sides.
+ */
+class Dice(private val numSides: Int) {
+
+    /**
+     * Do a random dice roll and return the result.
+     */
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
