521d66a17839321ad78fe0aee3be887bfdd6f6dd
jay_wu <jay_wu@gss.com.tw>
2021-01-09 18:41:34 +0800

    ✨ Finish CodeLab: Add images to the Dice Roller app

--- HappyBirthday/app/src/main/java/com/wjprogrammer/happybirthday/activities/diceRoller/DiceRollerActivity.kt
+++ HappyBirthday/app/src/main/java/com/wjprogrammer/happybirthday/activities/diceRoller/DiceRollerActivity.kt
@@ -3,6 +3,7 @@ package com.wjprogrammer.happybirthday.activities.diceRoller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.wjprogrammer.happybirthday.R
@@ -26,6 +27,9 @@ class DiceRollerActivity : AppCompatActivity() {
 
         // Set a click listener on the button to roll the dice when the user taps the button
         rollButton.setOnClickListener { rollDice() }
+
+        // Do a dice roll when the app starts
+        rollDice()
     }
 
     /**
@@ -44,5 +48,23 @@ class DiceRollerActivity : AppCompatActivity() {
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
 
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
